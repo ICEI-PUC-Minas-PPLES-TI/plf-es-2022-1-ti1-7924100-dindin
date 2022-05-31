b24e799395a3ca3fa01bb711f998f0cbb05b3ea1
SitaGomes <ansitagomes@gmail.com>
2022-05-31 23:58:11 +0000

    Add: mobile view completed

--- src/pages/overview/script.js
+++ src/pages/overview/script.js
@@ -1,20 +1,21 @@
 function closeMenu() {
-    const hamburguer = document.querySelector("#openMenu");
+    const main = document.querySelector("#main");
     const aside = document.querySelector("#aside")
 
     aside.classList.remove("open-menu");
     aside.classList.add("close-menu");
 
-    hamburguer.classList.remove("close-menu");
+    main.classList.remove("close-menu");
 }
 
 function openMenu() {
-    const hamburguer = document.querySelector("#openMenu");
+    //const hamburguer = document.querySelector("#openMenu");
+    const main = document.querySelector("#main");
     const aside = document.querySelector("#aside")
 
     aside.classList.remove("close-menu");
     aside.classList.add("open-menu");
-    hamburguer.classList.add("close-menu");
+    main.classList.add("close-menu");
 }
 
 
